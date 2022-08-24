2dc689eb3c83f73761fce3a4fee22eb26bc02884
Brandon Aldred <brandonaldred@gmail.com>
2022-08-24 05:32:57 -0700

    breaking more stuff

--- ecommerce-product-page-main/js/app.js
+++ ecommerce-product-page-main/js/app.js
@@ -1,8 +1,10 @@
+//Photo Scrolling Selecting All Elements
 const productImages = document.querySelector('.product-thumbnails').querySelectorAll('IMG');
 const changeImg = document.querySelector('.photo-scroll').querySelectorAll('BUTTON');
 const mainPhoto = document.querySelector('.main-photo');
 
 
+//Desktop Photo Selection
 for (image of productImages) {
     image.addEventListener('click', (e) => {
         mainPhoto.src = `images/image-${e.target.dataset.product}.jpg`;
@@ -14,12 +16,14 @@ for (image of productImages) {
 
 }
 
+//Mobile Photo Scrolling
 for (let i = 0; i < changeImg.length; i++) {
     changeImg[i].addEventListener('click', () => {
         if (changeImg[i].dataset.next == 'true') {
             if (parseInt(mainPhoto.dataset.photo) < productImages.length) {
                 mainPhoto.dataset.photo = parseInt(mainPhoto.dataset.photo) + 1;
                 mainPhoto.src = `images/image-product-${mainPhoto.dataset.photo}.jpg`;
+                
             }
         } else {
             if (parseInt(mainPhoto.dataset.photo) > 1) {
@@ -29,3 +33,19 @@ for (let i = 0; i < changeImg.length; i++) {
         }
     })
 }
+
+
+//Lightbox Elements
+const lightboxClose = document.querySelector('.close-lightbox');
+const lightboxPhotoContainer = document.getElementById('lightbox-photo-container');
+lightboxClose.addEventListener('click',() => {
+    document.getElementById('lightbox').style.display = 'none';
+    lightboxPhotoContainer.innerHTML = '';
+
+});
+
+mainPhoto.addEventListener('click', () => {
+    document.getElementById('lightbox').style.display = 'flex';
+    const photoContainer = document.querySelector('.photo-container');
+    lightboxPhotoContainer.appendChild(photoContainer.cloneNode(true));
+});
