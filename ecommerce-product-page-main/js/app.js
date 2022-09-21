87b530e0536036af830a2adcf0a20e48d338a000
Brandon Aldred <brandonaldred@gmail.com>
2022-09-21 06:05:18 -0700

    Updating all kinds of stuff

--- ecommerce-product-page-main/js/app.js
+++ ecommerce-product-page-main/js/app.js
@@ -2,6 +2,9 @@
 const productImages = document.querySelector('.product-thumbnails').querySelectorAll('IMG');
 const changeImg = document.querySelector('.photo-scroll').querySelectorAll('BUTTON');
 const mainPhoto = document.querySelector('.main-photo');
+const cartButton = document.querySelector('.shopping-cart');
+const cartContents = document.getElementById('cart-contents');
+
 
 
 //Desktop Photo Selection
@@ -23,7 +26,7 @@ for (let i = 0; i < changeImg.length; i++) {
             if (parseInt(mainPhoto.dataset.photo) < productImages.length) {
                 mainPhoto.dataset.photo = parseInt(mainPhoto.dataset.photo) + 1;
                 mainPhoto.src = `images/image-product-${mainPhoto.dataset.photo}.jpg`;
-                
+
             }
         } else {
             if (parseInt(mainPhoto.dataset.photo) > 1) {
@@ -38,7 +41,7 @@ for (let i = 0; i < changeImg.length; i++) {
 //Lightbox Elements
 const lightboxClose = document.querySelector('.close-lightbox');
 const lightboxPhotoContainer = document.getElementById('lightbox-photo-container');
-lightboxClose.addEventListener('click',() => {
+lightboxClose.addEventListener('click', () => {
     document.getElementById('lightbox').style.display = 'none';
     lightboxPhotoContainer.innerHTML = '';
 
@@ -49,3 +52,59 @@ mainPhoto.addEventListener('click', () => {
     const photoContainer = document.querySelector('.photo-container');
     lightboxPhotoContainer.appendChild(photoContainer.cloneNode(true));
 });
+
+
+//Hide or Show Shopping Cart
+cartButton.addEventListener('click', () => {
+    cartContents.className == 'hide' ? cartContents.className = 'cart-contents' : cartContents.className = 'hide';
+})
+
+//Add Items to Shopping Cart (product, qty, img, price)
+function addCartItem(product, title, qty, price) {
+    //Is cart empty? If so, remove empty-cart-text & add cart-item
+    let exists = false;
+    if (cartContents.querySelector('.empty-cart-text')) {
+        cartContents.querySelector('.empty-cart-text').remove()
+    }
+    //If cart is not empty, Check current cart items, does what we're adding exist?
+    if (cartContents.querySelector('.cart-item')) {
+        let cart = cartContents.querySelectorAll('.cart-item');
+        for (item of cart) {
+            product == item.dataset.product ? exists = true : exists = false
+        }
+    }
+    //If it does, update qty & price
+    console.log(exists);
+    //If not, add cart-item
+    if (!exists) { document.querySelector('.cart-items').appendChild(buildCartItem(product,title,qty,price)); }
+
+}
+
+function buildCartItem (product, title, qty, price) {
+    let cartItem = document.createElement('DIV');
+    cartItem.className = 'cart-item';
+    cartItem.dataset.product = product;
+    cartItem.dataset.qty = qty;
+    cartItem.dataset.price = price;
+
+    let itemImg = document.createElement('IMG');
+    itemImg.className = 'cart-thumbnail';
+    itemImg.src = `images/image-${product}-thumbnail.jpg`;
+    itemImg.setAttribute('alt', title);
+
+    let itemTitle = document.createElement('P');
+    itemTitle.innerHTML = title + '</br>$' + price + ' x ' + qty + '<b> $' + (qty * price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); + '</b>';
+
+    let removeItem = document.createElement('IMG');
+    removeItem.src = 'images/icon-delete.svg';
+    removeItem.className = 'cart-remove';
+    removeItem.setAttribute('alt', `Remove ${title} from cart`);
+
+    cartItem.appendChild(itemImg);
+    cartItem.appendChild(itemTitle);
+    cartItem.appendChild(removeItem);
+    return cartItem;
+
+}
+addCartItem('product-1', 'sneakers', 8, 150);
+addCartItem('product-8', 'shirts', 1, 15);
